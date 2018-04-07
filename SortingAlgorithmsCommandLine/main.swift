e8c383ea555a0020d4ec6bf044265caefbfa949a
Umai Z <mzhusupbekova@lcs.on.ca>
2018-04-07 14:54:38 -0400

    used function "sort" in order to sort numbers in array

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -22,11 +22,13 @@ func random(between min: UInt32, and max: UInt32) -> Int {
 
 // populate the array
 var unsorted : [Int] = []
-var sorted : [Int] = []
+// var sorted : [Int] = []
+
 
 //for _ in 1...7 {
 //    unsorted.append( random(between: 1, and: 14) )
 //}
+
 unsorted.append(7)
 unsorted.append(3)
 unsorted.append(10)
@@ -34,35 +36,43 @@ unsorted.append(6)
 unsorted.append(8)
 
 
+
+unsorted.sort(by: >) // the function sort compares variables and put them in right placement
+print(unsorted) // print result
+
+
+
+
+
 // keep looping until the unsorted list is empty
-while unsorted.count > 0 {
-    
-    // takes a finale number of unsorted list
-    // like in cards takes the bottom
-    var numberToBeInserted = unsorted[unsorted.count-1]
-    unsorted.remove(at: unsorted.count - 1)   // Remove number at end of the array
-    
-    // start to populate sorting list
-    if sorted.count == 0 {
-        sorted.append (numberToBeInserted)
-        
-    } else {
-       
-        for i in 0...sorted.count - 1 {  //loop to compare
-            // takes the next unsorted number from the bottom
-            if numberToBeInserted > sorted[i]  {  // if unsorted bigger than sorted
-                sorted.insert(numberToBeInserted, at: i)
-                break
-            } else { // if less than sorted moves to the right (basically down after highest number)
-                sorted.append(numberToBeInserted ) // inserted
-                break //to stop the loop, so it wont go one number forever
-            }
-        }
-    }
-    
-    }
-    
-    
-    
-    
+//while unsorted.count > 0 {
+//
+//    // takes a finale number of unsorted list
+//    // like in cards takes the bottom
+//    var numberToBeInserted = unsorted[unsorted.count-1]
+//    unsorted.remove(at: unsorted.count - 1)   // Remove number at end of the array
+//
+//    // start to populate sorting list
+//    if sorted.count == 0 {
+//        sorted.append (numberToBeInserted)
+//
+//    } else {
+//
+//
+//        // set a flag to tell if the number was inserted
+//        var numberWasInserted = false
+//
+//        for i in 0...sorted.count - 1 {  //loop to compare
+//            // takes the next unsorted number from the bottom
+//            if numberToBeInserted > sorted[i]  {  // if unsorted bigger than sorted
+//                sorted.insert(numberToBeInserted, at: i)
+//                numberWasInserted = true
+//                break
+// }
+//}
+//}
+//}
+
+
+
 
